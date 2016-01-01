854d6a5e0ed9f7648d761bfed6e9fc8cb0c46f23
Mikhail Grinshteyn <mgrinsht@gmail.com>
2016-01-01 12:14:50 -0500

    add target files

---
+++ target/js/controllers/templates.js
@@ -0,0 +1,16 @@
+﻿angular.module('prerial').run(['$templateCache', function ($templateCache) {
+    'use strict';
+
+    $templateCache.put('templates/modal.html',
+      "<div class=\"pre-window-wrapper\">" +
+      "<div class=\"pre-window\"><button ng-click=\"hide()\" class=\"close pre-icon-close-box\" type=\"button\"></button>" +
+      "<div class=\"pre-window-header\"><h3>Modal window</h3></div><div class=\"pre-window-body\"></div>" +
+      "<div class=\"pre-window-footer\" ng-click=\"hide()\" style='height:50px'><button class=\"btn apply btn-primary\">OK</button> <button class=\"btn cancel btn-secondary\">Cancel</button></div>" +
+      "</div></div>"
+    );
+
+    $templateCache.put('templates/modalbody.html',
+      "<div style='width:200px;height:60px;margin:20px'>Hello Modal!</div>"
+    );
+
+}]);
