6117de8db674c83f7565ce4cb9740c2f7f053eff
prerial <mgrinsht@gmail.com>
2016-01-31 07:06:15 -0500

    accordion

---
+++ src/accordion/accordion-svc.js
@@ -0,0 +1,30 @@
+/**
+ * Created by Mikhail on 1/31/2016.
+ */
+(function () {
+    'use strict';
+
+    function accordionService() {
+
+        return {
+
+            panes:{},
+
+            addPane: function(pane) {
+                this.panes[pane.idx] = pane;
+            },
+
+            togglePane: function(idx) {
+                for(var key in this.panes) {
+                    if (this.panes.hasOwnProperty(key)) {
+                        this.panes[key].scope.accordionPaneShow = false;
+                    }
+                }
+                this.panes[idx].scope.accordionPaneShow = !this.panes[idx].scope.accordionPaneShow;
+            }
+        };
+    }
+
+    angular.module('prerial').factory('accordionService', accordionService);
+
+})();
